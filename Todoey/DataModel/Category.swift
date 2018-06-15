68656b2ad8e38f01195f2b6a0fe95836b9a4ac4d
Jason Rossello <jasonr@macbook-air-de-jason.local>
2018-06-14 20:10:42 -0500

    Persisting data using Realm

---
+++ Todoey/DataModel/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by jason rossello on 6/13/18.
+//  Copyright © 2018 jason rossello. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
